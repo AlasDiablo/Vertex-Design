4eb2dc0b41a82d3c9d828e37c3fb0c50c522f841
AlasDiablo <25723276+AlasDiablo@users.noreply.github.com>
2024-02-16 16:16:42 +0100

    refactor: update css variable injection and add stylelint

--- src/provider/VertexDesignThemeProvider.tsx
+++ src/provider/VertexDesignThemeProvider.tsx
@@ -1,23 +1,21 @@
-import { useEffect } from 'react';
+import { createGlobalStyle } from 'styled-components';
 import type { PropsWithChildren } from 'react';
 
 export type VertexDesignThemeProviderProps = PropsWithChildren;
 
-const CssVariableInjector = () => {
-    useEffect(() => {
-        const style = document.documentElement.style;
-        style.setProperty('--vertex-border-color', 'rgba(0, 255, 255, 0.75)');
-        style.setProperty('--vertex-grey-89', '#dee2e6');
-        style.setProperty('--vertex-grey-85', '#d3d9de');
-        style.setProperty('--vertex-grey-75', '#b6bfc8');
-    }, []);
-    return null;
-};
+const GlobalStyle = createGlobalStyle`
+    :root {
+        --vertex-border-color: rgb(0 255 255 / 75%);
+        --vertex-grey-89: #dee2e6;
+        --vertex-grey-85: #d3d9de;
+        --vertex-grey-75: #b6bfc8;
+    }
+`;
 
 const VertexDesignThemeProvider = ({ children }: VertexDesignThemeProviderProps) => {
     return (
         <>
-            <CssVariableInjector />
+            <GlobalStyle />
             {children}
         </>
     );
